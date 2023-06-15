3bb01e7caa6813cce2992ec3741d2e278aa6442b
mozezhao <mozezhao@moresec.cn>
2023-06-15 16:43:53 +0800

    finish engine handle

--- controllers/v1/engine.go
+++ controllers/v1/engine.go
@@ -1,1 +1,14 @@
 package v1
+
+import (
+	"ir-web/internal/wrapper"
+	v1_req "ir-web/models/protoreq/v1"
+	v1 "ir-web/services/v1"
+)
+
+type EngineController struct {
+}
+
+func (EngineController) Query(ctx *wrapper.Context) {
+	wrapper.ApiWrapper(ctx, v1.Query, true, &v1_req.EngineRequest{}, nil)
+}
