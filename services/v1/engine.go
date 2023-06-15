488c59ed2833fa7fd68a84905c5acc0f90350cb6
mozezhao <mozezhao@moresec.cn>
2023-06-15 13:50:12 +0800

    add engine init

--- services/v1/engine.go
+++ services/v1/engine.go
@@ -1,1 +1,31 @@
 package v1
+
+import (
+	"ir-web/conf"
+	mlog "ir-web/internal/wrapper/mlog"
+)
+
+type InvertedIndex map[string][]int
+
+type Engine struct {
+	Terms                  map[int][]string
+	CosineSimilarityMatrix map[int]map[int]float64
+	VocabularySet          map[string]bool
+	PostingList            InvertedIndex
+	Documents              []string
+	TfIdfMatrix            map[int]map[string]float64
+}
+
+var engine *Engine
+
+func GetEngine() *Engine {
+	if engine == nil {
+		mlog.Error("init engine first")
+		InitEngineWithConfig(conf.GetConfig().Conf)
+		if engine == nil {
+			panic("init engine error")
+		}
+		return engine
+	}
+	return engine
+}
