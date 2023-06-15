7212cdf135b5f8f6ab984b2b1d327d0de763b3ba
mozezhao <mozezhao@moresec.cn>
2023-06-15 10:33:26 +0800

    init router

---
+++ routers/router.go
@@ -0,0 +1,29 @@
+package routers
+
+import (
+	mlog "ir-web/internal/wrapper/mlog"
+	// v1 "oa-review/routers/api/v1"
+
+	"github.com/kataras/iris/v12"
+)
+
+type IrisRouter struct{}
+
+func (IrisRouter) InitApp(app *iris.Application) {
+	mlog.Info("init app")
+	loadMiddlerware(app)
+	// appRouter := app.Party("/")
+	// {
+	// 	// 注册健康检查路由
+	// 	v1.RegisterHealthRouter(appRouter)
+
+	// 	// 注册User路由 包括普通用户以及审核用户
+	// 	v1.RegisterUserRouter(appRouter)
+	// }
+	// logger.Info("init app success")
+}
+
+func loadMiddlerware(app *iris.Application) {
+	// logger.Info("load middle ware for app")
+	//app.Use()
+}
